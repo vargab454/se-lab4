e93f700f97eacaba1881500dd67114869f235625
Varga Bálint <varga.balint20040504@gmail.com>
2024-11-06 12:04:58 +0100

    Added unit tests for TorpedoStore class

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -1,6 +1,7 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +17,36 @@ class TorpedoStoreTest {
 
         // Assert
         assertEquals(true, result);
+    }
+
+    @Test
+    void fire_Fail() {
+        TorpedoStore store = new TorpedoStore(1,1);
+        assertEquals(false, store.fire(1));
+    }
+
+    @Test
+    void fire_Throw() {
+        TorpedoStore store = new TorpedoStore(1);
+        assertThrows(IllegalArgumentException.class, () -> { store.fire(-1); } );
+        assertThrows(IllegalArgumentException.class, () -> { store.fire(10); } );
+    }
+
+    @Test
+    void isEmptyTest() {
+        TorpedoStore store = new TorpedoStore(1,0);
+        assertEquals(false, store.isEmpty());
+        store.fire(1);
+        assertEquals(true, store.isEmpty());
+    }
+
+    @Test
+    void getTorpedoCountTest() {
+        TorpedoStore store = new TorpedoStore(3,0);
+        assertEquals(3, store.getTorpedoCount());
+        store.fire(2);
+        assertEquals(1, store.getTorpedoCount());
+        store.fire(1);
+        assertEquals(0, store.getTorpedoCount());
     }
 }
